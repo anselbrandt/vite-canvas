4bdf3e59de37d0bdb695bab198ce14a4f29a0559
ansel brandt <ansel@anselbrandt.com>
2022-07-11 21:38:35 -0400

    Update styling

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
   }, [video, canvas]);
 
   return (
-    <div>
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
       <div>
         <video
           ref={setVideoRef}
